d296d38ec569bf18fec96f5b78173eb17b7915a3
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-21 14:53:49 +0200

    Update HomePrincipale.vue

--- frontend/src/views/HomePrincipale.vue
+++ frontend/src/views/HomePrincipale.vue
@@ -4,7 +4,6 @@
     
     <nav class="navbar navbar-expand-lg bg-body-tertiary mt-3">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">ToDo APP</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
