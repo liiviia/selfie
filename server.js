efd4eaa928ab5e1328bc87d8b574709c1a6e6c2f
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 12:09:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,38 +4,32 @@ const connectDB = require('./config/dbConnection');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const noteRoutes = require('./routes/noteRoutes');
-const pomsRoutes= require ('./routes/pomsRouter');
-const eventRoutes = require ('./routes/eventRoutes') ;
-const activityRoutes = require('./routes/activityRoutes') ;
-const registerRoutes = require('./routes/registerRoutes'); 
+const pomsRoutes = require('./routes/pomsRouter');
+const eventRoutes = require('./routes/eventRoutes');
+const activityRoutes = require('./routes/activityRoutes');
+const registerRoutes = require('./routes/registerRoutes');
+const { scheduleEmailReminders } = require('./cronTask'); 
 
 const app = express();
 const port = 3000;
 
-
 connectDB();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/api/auth', authRoutes);
-
 app.use('/api', noteRoutes);
-
-app.use('/api' , pomsRoutes) ;
-
-app.use('/api' ,eventRoutes) ;
-
-app.use('/api', activityRoutes) ;
-
-app.use('/api' , registerRoutes) ;
-
+app.use('/api', pomsRoutes);
+app.use('/api', eventRoutes);
+app.use('/api', activityRoutes);
+app.use('/api', registerRoutes);
 app.use(express.static(path.join(__dirname, 'frontend/frontend/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/frontend/dist', 'index.html'));
 });
+
+scheduleEmailReminders(); 
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
