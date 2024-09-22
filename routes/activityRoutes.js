26534059bc56a00c146efc0f76caa5fd3aa845bc
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 12:09:24 +0200

    Update activityRoutes.js

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const Activity = require('../models/Activity'); 
-
+const { sendReminderEmail } = require('../emailService'); 
 
 router.post('/activities', async (req, res) => {
   try {
-    const { title, description, deadline, author, completed } = req.body;
+    const { title, description, deadline, author, email, completed } = req.body;
     
     const newActivity = new Activity({
       title,
       description,
       deadline,
       author,
+      email,
       completed
     });
 
     const savedActivity = await newActivity.save();
+
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    
+    if (new Date(deadline).setHours(0, 0, 0, 0) === tomorrow.setHours(0, 0, 0, 0)) {
+      sendReminderEmail(email, title, deadline); 
+    }
+
     res.status(201).json(savedActivity);
   } catch (error) {
     console.error('Errore durante il salvataggio dell\'attività:', error);
@@ -23,17 +32,18 @@ router.post('/activities', async (req, res) => {
   }
 });
 
+router.get('/activitiesGET', async (req, res) => {
+  try {
+    const username = req.query.username;
 
-router.get('/activities', async (req, res) => {
-    try {
-      const activities = await Activity.find(); 
-      res.status(200).json(activities); 
-    } catch (error) {
-      res.status(500).json({ error: 'Errore durante il recupero delle attività' });
+    if (!username) {
+      return res.status(400).json({ message: 'Username è necessario' });
     }
-  });
-
-
-  
+    const activities = await Activity.find({author: username }); 
+    res.status(200).json(activities); 
+  } catch (error) {
+    res.status(500).json({ error: 'Errore durante il recupero delle attività' });
+  }
+});
 
 module.exports = router;
