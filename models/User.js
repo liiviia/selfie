654cccc081f15d19b7e3c8d2d708f07b405d8ea2
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 12:44:25 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
@@ -6,5 +7,9 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     phoneNumber: { type: String, required: true }          
 });
+
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
