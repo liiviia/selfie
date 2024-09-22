158361cb441f91f6d6f0725f40f2d180fd0a5082
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 15:20:58 +0200

    Update activityRoutes.js

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Activity = require('../models/Activity'); 
 const { sendReminderEmail } = require('../emailService'); 
+const { route } = require('./eventRoutes');
 
 router.post('/activities', async (req, res) => {
   try {
@@ -44,6 +45,24 @@ router.get('/activitiesGET', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Errore durante il recupero delle attività' });
   }
+});
+
+
+router.get('/activities/last', async (req, res) => {
+  try {
+    const username = req.query.username;
+
+    if(!username) {
+      return res.status(400).json({message: 'Username è necessario' }) ;
+    }
+
+    const activity = await Activity.findOne({author: username}) ;
+    res.json(activity);
+
+} catch (error) {
+  console.error('errore nel recupero dell evento',error);
+  res.status(500).json({ error: 'Errore nel recupero degli eventi' });
+}
 });
 
 module.exports = router;
