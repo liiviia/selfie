0429dd380b863cbea43e8f5b71aa4a1c910d75ee
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 16:04:28 +0200

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
+
 
 router.put('/updatePassword', async (req, res) => {
     const { username, currentPassword, newPassword } = req.body;
@@ -29,6 +31,18 @@ router.put('/updatePassword', async (req, res) => {
 
 
 
+const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false, 
+    auth: {
+      user: 'selfieAPP@outlook.it',
+      pass: 'APPselfie2024',
+    },
+  });
+  
+
+
 router.delete('/deleteAccount', async (req, res) => {
     const { username } = req.body;
 
@@ -38,11 +52,28 @@ router.delete('/deleteAccount', async (req, res) => {
             return res.status(404).json({ message: 'Utente non trovato' });
         }
 
+        
+        const mailOptions = {
+            from: '"Servizio Sito SELFIE"<selfieAPP@outlook.it>',
+            to: user.email, 
+            subject: 'Account Eliminato',
+            text: 'Il tuo account è stato eliminato con successo.'
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Errore durante l\'invio dell\'email:', error);
+            } else {
+                console.log('Email inviata: ' + info.response);
+            }
+        });
+
         res.status(200).json({ message: 'Account eliminato con successo' });
     } catch (error) {
         console.error('Errore durante l\'eliminazione dell\'account:', error);
         res.status(500).json({ message: 'Errore interno del server' });
     }
 });
+
 
 module.exports = router;
