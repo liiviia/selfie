c7fb1ff343963edc45fb5886c11ced7dc77a3c5d
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-09-22 11:06:05 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    phoneNumber: { type: String, required: true }          
 });
 
 module.exports = mongoose.model("User", userSchema);
