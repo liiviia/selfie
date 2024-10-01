aace6d32976dfbc75320c1c368d9fa428b02022b
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:42:15 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,38 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User'); 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const verifyToken = require('../middleware/authenticateToken'); 
+const authController = require('../controllers/authControllers');
 
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(401).json({ message: 'Username o password errati' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Username o password errati' });
-    }
-
-    const token = jwt.sign(
-      { id: user._id, username: user.username },
-      'yourSecretKey', 
-      { expiresIn: '1h' }
-    );
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Errore del server' });
-  }
-});
-
-
+router.post('/login', authController.login);
 
 module.exports = router;
