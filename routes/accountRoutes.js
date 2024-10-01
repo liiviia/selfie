3778fed6ad385004b84b9db33ec916caf36143f1
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:41:36 +0200

    Update accountRoutes.js

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,79 +1,10 @@
 const express = require('express');
-const User = require('../models/User');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const nodemailer = require('nodemailer');
+const accountController = require('../controllers/accountController');
+const authenticateToken = require('../middleware/authenticateToken');
 
+router.put('/updatePassword', authenticateToken, accountController.updatePassword);
 
-router.put('/updatePassword', async (req, res) => {
-    const { username, currentPassword, newPassword } = req.body;
-
-    try {
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(404).json({ message: 'Utente non trovato' });
-        }
-
-        const isMatch = await user.comparePassword(currentPassword);
-        if (!isMatch) {
-            return res.status(401).json({ message: 'Password attuale errata' });
-        }
-
-        user.password = await bcrypt.hash(newPassword, 10);
-        await user.save();
-
-        res.status(200).json({ message: 'Password aggiornata con successo' });
-    } catch (error) {
-        console.error('Errore durante l\'aggiornamento della password:', error);
-        res.status(500).json({ message: 'Errore interno del server' });
-    }
-});
-
-
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: false, 
-    auth: {
-      user: 'selfieAPP@outlook.it',
-      pass: 'APPselfie2024',
-    },
-  });
-  
-
-
-router.delete('/deleteAccount', async (req, res) => {
-    const { username } = req.body;
-
-    try {
-        const user = await User.findOneAndDelete({ username });
-        if (!user) {
-            return res.status(404).json({ message: 'Utente non trovato' });
-        }
-
-        
-        const mailOptions = {
-            from: '"Servizio Sito SELFIE"<selfieAPP@outlook.it>',
-            to: user.email, 
-            subject: 'Account Eliminato',
-            text: 'Il tuo account è stato eliminato con successo.'
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Errore durante l\'invio dell\'email:', error);
-            } else {
-                console.log('Email inviata: ' + info.response);
-            }
-        });
-
-        res.status(200).json({ message: 'Account eliminato con successo' });
-    } catch (error) {
-        console.error('Errore durante l\'eliminazione dell\'account:', error);
-        res.status(500).json({ message: 'Errore interno del server' });
-    }
-});
-
+router.delete('/deleteAccount', authenticateToken, accountController.deleteAccount);
 
 module.exports = router;
