43ad41cd9b24d11c2dcfe8d527c6e65bbe1a813a
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:42:46 +0200

    Update noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const noteController = require('../controllers/noteController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.get('/notesGET', noteController.getAllNotes);
-router.get('/nota/:id', noteController.getNoteById);
+router.get('/notesGET' ,authenticateToken, noteController.getAllNotes);
+router.get('/nota/:id', authenticateToken, noteController.getNoteById);
 
 
 
-router.post('/notes',noteController.createNote);
+router.post('/notes',authenticateToken, noteController.createNote);
 
-router.put('/notes/:id', noteController.updateNote);
+router.put('/notes/:id',authenticateToken, noteController.updateNote);
 
-router.delete('/notesRIM/:id', noteController.deleteNote);
+router.delete('/notesRIM/:id',authenticateToken, noteController.deleteNote);
 
 
 
