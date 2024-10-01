3f70b09e83b6282fa43a17097a6498807870c1f2
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:43:02 +0200

    Update pomsRouter.js

--- routes/pomsRouter.js
+++ routes/pomsRouter.js
@@ -1,42 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Pom = require('../models/pom'); 
+const pomController = require('../controllers/pomsController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.post('/pomsPOST', async (req, res) => {
-  try {
-    const { username, tempoStudio, tempoPausa, ripetizioni } = req.body;
-    const newPom = new Pom({
-      username,
-      tempoStudio,
-      tempoPausa,
-      ripetizioni
-    });
+router.post('/pomsPOST', authenticateToken, pomController.createPom);
 
-    const savedPom = await newPom.save();
-    res.status(201).json(savedPom);
-  } catch (error) {
-    console.error('Errore durante il salvataggio:', error);
-    res.status(500).json({ error: error.message });
-}
-
-});
-
-
-router.get('/poms', async (req, res) => {
-    try {
-      const username = req.query.username;
-
-      if (!username) {
-        return res.status(400).json({ message: 'Username è necessario' });
-      }
-
-      const poms = await Pom.find({username: username });
-      res.status(200).json(poms);
-    } catch (error) {
-      res.status(500).json({ error: 'Errore durante il recupero delle sessioni Pomodoro' });
-    }
-  });
-
-
+router.get('/poms', authenticateToken, pomController.getPoms);
 
 module.exports = router;
