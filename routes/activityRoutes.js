367bd900508197444b854c8a009ba51aed2945f0
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:41:55 +0200

    Update activityRoutes.js

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,68 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Activity = require('../models/Activity'); 
-const { sendReminderEmail } = require('../emailService'); 
-const { route } = require('./eventRoutes');
+const activityController = require('../controllers/activityController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.post('/activities', async (req, res) => {
-  try {
-    const { title, description, deadline, author, email, completed } = req.body;
-    
-    const newActivity = new Activity({
-      title,
-      description,
-      deadline,
-      author,
-      email,
-      completed
-    });
+router.post('/activities', authenticateToken, activityController.createActivity);
 
-    const savedActivity = await newActivity.save();
+router.get('/activitiesGET', authenticateToken, activityController.getActivities);
 
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    
-    if (new Date(deadline).setHours(0, 0, 0, 0) === tomorrow.setHours(0, 0, 0, 0)) {
-      sendReminderEmail(email, title, deadline); 
-    }
-
-    res.status(201).json(savedActivity);
-  } catch (error) {
-    console.error('Errore durante il salvataggio dell\'attività:', error);
-    res.status(500).json({ error: 'Errore durante il salvataggio dell\'attività' });
-  }
-});
-
-router.get('/activitiesGET', async (req, res) => {
-  try {
-    const username = req.query.username;
-
-    if (!username) {
-      return res.status(400).json({ message: 'Username è necessario' });
-    }
-    const activities = await Activity.find({author: username }); 
-    res.status(200).json(activities); 
-  } catch (error) {
-    res.status(500).json({ error: 'Errore durante il recupero delle attività' });
-  }
-});
-
-
-router.get('/activities/last', async (req, res) => {
-  try {
-    const username = req.query.username;
-
-    if(!username) {
-      return res.status(400).json({message: 'Username è necessario' }) ;
-    }
-
-    const activity = await Activity.findOne({author: username}) ;
-    res.json(activity);
-
-} catch (error) {
-  console.error('errore nel recupero dell evento',error);
-  res.status(500).json({ error: 'Errore nel recupero degli eventi' });
-}
-});
+router.get('/activities/last', authenticateToken, activityController.getLastActivity);
 
 module.exports = router;
