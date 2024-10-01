3ba642c83abddb0fe9c53b00b81e5b1d45df8ee5
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:42:30 +0200

    Update eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,89 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/Event');
+const eventController = require('../controllers/eventController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.post('/events', async (req, res) => {
-  try {
-    const {
-      title,
-      description,
-      date,
-      startTime,
-      duration,
-      isRecurring,
-      frequency,
-      recurrencePattern,
-      numberOfOccurrences,
-      location,
-      author,
-      notificationMechanism,
-      notificationTime,
-      repeatNotification,
-    } = req.body;
+router.post('/events', authenticateToken, eventController.createEvent);
 
-    let notificationMechanismArray = [];
+router.get('/eventsGET', authenticateToken, eventController.getEvents);
 
-    if (Array.isArray(notificationMechanism)) {
-      notificationMechanismArray = notificationMechanism; 
-    } else if (typeof notificationMechanism === 'string') {
-      notificationMechanismArray = notificationMechanism.split(','); 
-    }
-
-    const newEvent = new Event({
-      title,
-      description,
-      date,
-      startTime,
-      duration,
-      isRecurring,
-      frequency,
-      recurrencePattern,
-      numberOfOccurrences,
-      location,
-      author,
-      notificationMechanism: notificationMechanismArray, 
-      notificationTime,
-      repeatNotification,
-    });
-
-    const savedEvent = await newEvent.save();
-    res.status(201).json(savedEvent);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Errore durante l\'aggiunta dell\'evento' });
-  }
-});
-
-router.get('/eventsGET', async (req, res) => {
-  try {
-    const author = req.query.author;
-
-    if (!author) {
-      return res.status(400).json({ message: 'Autore è necessario' });
-    }
-
-    const events = await Event.find({ author });
-    res.json(events);
-  } catch (error) {
-    console.error('Errore nel recupero degli eventi:', error);
-    res.status(500).json({ error: 'Errore nel recupero degli eventi' });
-  }
-});
-
-router.get('/events/last', async (req, res) => {
-  try {
-    const author = req.query.author;
-
-    if (!author) {
-      return res.status(400).json({ message: 'Autore è necessario' });
-    }
-
-    const event = await Event.findOne({ author }).sort({ createdAt: -1 }); 
-    res.json(event);
-  } catch (error) {
-    console.error('Errore nel recupero dell\'evento', error);
-    res.status(500).json({ error: 'Errore nel recupero degli eventi' });
-  }
-});
+router.get('/events/last', authenticateToken, eventController.getLastEvent);
 
 module.exports = router;
