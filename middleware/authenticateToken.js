2c04b07d2faff8294cb0bb608e280d57807cc904
Massimiliano1231 <82729355+Massimiliano1231@users.noreply.github.com>
2024-10-01 12:44:05 +0200

    Update authenticateToken.js

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -3,14 +3,20 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+  console.log("token backend:", token);
 
-  if (!token) return res.status(401).json({ message: 'Accesso negato' });
+  if (!token) {
+    return res.status(403).json({ message: 'Token mancante, accesso negato' });
+  }
 
-  jwt.verify(token, 'your_jwt_secret', (err, user) => {
-    if (err) return res.status(403).json({ message: 'Token non valido' });
 
-    req.user = user;
-    next();
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token non valido' });
+    }
+
+    req.user = user; 
+    next(); 
   });
 };
 
