13359700528746d7863bccb1eebb690b80298f96
Massimiliano Fioravanti - massimili.fioravant2@studio.unibo.it <massimili.fioravant2@labs.cs.unibo.it>
2024-11-19 15:28:04 +0100

    e

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoDBUri = "mongodb://siteUser:pswPerMongo@127.0.0.1:27017/site232432";
 const connectDB = async () => {
     let debug = [];
     try {   
-           try {
+          
     console.log("Inizio connessione...");
     await mongoose.connect(mongoDBUri);
     console.log("Connesso con successo!");
