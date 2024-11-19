76ba7062e3c41a858d11fa1b6e5bc3fc72157248
massiFiora <massifioravanti@gmail.com>
2024-11-19 15:37:55 +0100

    k

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,44 +2,46 @@ const mongoose = require("mongoose");
 const mongoDBUri = "mongodb://siteUser:pswPerMongo@127.0.0.1:27017/site232432";
 const connectDB = async () => {
     let debug = [];
-    try {   
-
-    console.log("Inizio connessione...");
-    await mongoose.connect(mongoDBUri);
-    console.log("Connesso con successo!");
-} catch (err) {
-    console.error("Errore aaaaaaaa:", err.message);
-}
-
-
-        debug.push(`Attempting to connect to MongoDB with URI: ${mongoDBUri}`);
- /*       console.log("connettendosi al db");
+    try {
+        console.log("Inizio connessione...");
+        debug.push("Inizio connessione...");
         await mongoose.connect(mongoDBUri);
-        console.log("connesso");
-        debug.push("Successfully connected to MongoDB");*/
+        console.log("Connesso con successo!");
+        debug.push("Connesso con successo!");
+    } catch (err) {
+        console.error("Errore aaaaaaaa:", err.message);
+        debug.push(`Errore durante la connessione: ${err.message}`);
+        return { message: "Errore durante la connessione", debug };
+    }
 
-        mongoose.connection.on("connected", () =>{
-      console.log("Connected to MongoDB");
- debug.push("Connected to MongoDB");});
-        mongoose.connection.on("disconnected", () => debug.push("Disconnected from MongoDB"));
-        mongoose.connection.on("error", (err) => debug.push(`MongoDB connection error: ${err}`));
+    mongoose.connection.on("connected", () => {
+        console.log("Connesso a MongoDB");
+        debug.push("Connesso a MongoDB");
+    });
 
-        process.on("SIGINT", () => {
-            mongoose.connection.close()
-                .then(() => {
-                    debug.push("MongoDB connection closed");
-                    process.exit(0);
-                })
-                .catch(err => {
-                    debug.push(`Error closing MongoDB connection: ${err}`);
-                    process.exit(1);
-                });
-        });
+    mongoose.connection.on("disconnected", () => {
+        console.log("Disconnesso da MongoDB");
+        debug.push("Disconnesso da MongoDB");
+    });
 
-        return { message: "Connected to MongoDB", debug };
-    
+    mongoose.connection.on("error", (err) => {
+        console.error(`Errore di connessione MongoDB: ${err}`);
+        debug.push(`Errore di connessione MongoDB: ${err}`);
+    });
 
+    process.on("SIGINT", () => {
+        mongoose.connection.close()
+            .then(() => {
+                debug.push("Connessione MongoDB chiusa");
+                process.exit(0);
+            })
+            .catch(err => {
+                debug.push(`Errore durante la chiusura della connessione: ${err}`);
+                process.exit(1);
+            });
+    });
+
+    return { message: "Connesso a MongoDB", debug };
 };
-
 
 module.exports = connectDB;
