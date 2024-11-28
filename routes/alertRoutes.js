d0e258cacaf80806daf1f21ac20389659b43ae9c
massiFiora <massifioravanti@gmail.com>
2024-11-28 16:09:43 +0100

    a

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -5,4 +5,19 @@ const { getLatestAlerts } = require('../controllers/alertController');
 // Rotta per ottenere gli alert tramite polling
 router.get('/get-latest-alert', getLatestAlerts);
 
+router.get('/send-test-alert', (req, res) => {
+    // Dati dell'alert di prova
+    const testAlert = {
+      title: 'Alert di prova',
+      date: new Date(),
+      startTime: new Date().toLocaleTimeString(),
+      userNome: 'Test User',
+    };
+  
+    // Aggiungi l'alert
+    addAlert(testAlert.title, testAlert.date, testAlert.startTime, testAlert.userNome);
+  
+    res.json({ message: 'Alert di prova inviato!' });
+  });
+
 module.exports = router;
