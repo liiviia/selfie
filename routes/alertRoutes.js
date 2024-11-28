1c048c71705127c85a0a1f39b9e03690abec785c
massiFiora <massifioravanti@gmail.com>
2024-11-28 16:28:29 +0100

    a

--- routes/alertRoutes.js
+++ routes/alertRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getLatestAlerts } = require('../controllers/alertController');
+const { getLatestAlerts , addAlert} = require('../controllers/alertController');
 
 // Rotta per ottenere gli alert tramite polling
 router.get('/get-latest-alert', getLatestAlerts);
