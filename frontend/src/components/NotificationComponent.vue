99402f8c116e2f7d7822aa61e251c8652c3d64ab
massiFiora <massifioravanti@gmail.com>
2024-11-29 10:32:56 +0100

    a

--- frontend/src/components/NotificationComponent.vue
+++ frontend/src/components/NotificationComponent.vue
@@ -15,39 +15,42 @@ export default {
     this.startPolling();  
   },
   methods: {
-    startPolling() {
-      setInterval(() => {
-        fetch('/api/get-latest-alert')
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Errore nella risposta');
-            }
-            return response.json();  
-          })
-          .then(data => {
-            if (data.alerts && data.alerts.length > 0) {
-              const username = localStorage.getItem('username');  
-              
-              const filteredAlerts = data.alerts.filter(alert => alert.userNome.toLowerCase() === username.toLowerCase());
-             // console.log('Alert filtrati:', filteredAlerts); 
+  startPolling() {
+    const token = sessionStorage.getItem('token');
+    setInterval(() => {
+      fetch('/api/get-latest-alert', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Errore nella risposta');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.alerts && data.alerts.length > 0) {
+          const username = localStorage.getItem('username');
+          const filteredAlerts = data.alerts.filter(alert => alert.userNome.toLowerCase() === username.toLowerCase());
 
-              if (filteredAlerts.length > 0) {
-                this.alerts = filteredAlerts;  
-                filteredAlerts.forEach(alertItem => {
-                 alert(`Evento: ${alertItem.title}\nData: ${new Date(alertItem.date).toLocaleDateString()}\nOrario: ${alertItem.startTime}`);
-        });
-
-              }
-            } else {
-              //console.log('Nessun alert trovato.');
-            }
-          })
-          .catch(err => {
-            console.error('Errore nel polling:', err);
-          });
-      }, 5000); 
-    }
+          if (filteredAlerts.length > 0) {
+            this.alerts = filteredAlerts;
+            filteredAlerts.forEach(alertItem => {
+              alert(`Evento: ${alertItem.title}\nData: ${new Date(alertItem.date).toLocaleDateString()}\nOrario: ${alertItem.startTime}`);
+            });
+          }
+        }
+      })
+      .catch(err => {
+        console.error('Errore nel polling:', err);
+      });
+    }, 5000); // Intervallo di 5 secondi
   }
+}
+
 };
 </script>
 
