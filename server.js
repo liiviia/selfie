f63e0715ad07267b650711a71051f5321bb17dd5
= <=>
2025-01-07 20:40:55 +0100

    server

--- server.js
+++ server.js
@@ -15,12 +15,7 @@ const timeMachineRoutes = require('./routes/timeMachineRoutes');
 const timeMachineConfig = require('./timeMachineConfig');
 const alertRoutes = require('./routes/alertRoutes');
 const Pom = require('./models/pom'); 
-<<<<<<< HEAD
 const {initializeScheduler} = require('./scheduler.js'); 
-=======
-const { checkPomodoroAgainstTimeMachine } = require('./scheduler');
-
->>>>>>> 8cd6bfd994d72ff4a7a1c6f99d7f6d1e7849df8c
 
 // Aggiungi la rotta per gli alert
 const moment = require('moment-timezone');
@@ -50,7 +45,7 @@ app.use(cors({
 
 connectDB();
 
-//initializeScheduler(); 
+initializeScheduler(); 
 
 const incrementTimeMachine = () => {
   const currentTime = moment(timeMachineConfig.getTimeMachineDate()).tz('Europe/Rome');
